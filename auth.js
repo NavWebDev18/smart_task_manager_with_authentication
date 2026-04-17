c701dfd13d2ce90e48a0f98f8e57620f12121102
NavWebDev18 <naveenyadavpoluboina80@gmail.com>
2026-04-17 15:58:49 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -11,10 +11,97 @@ function saveToLocalStorage(usersList){
     localStorage.setItem("usersList",JSON.stringify(usersList));
 }
 // Registration Form logic
+
+function isValidUserName(str){
+
+    if (str.length < 8 || str.length > 29) {
+    return false;
+  }
+
+  let hasLower = false;
+  let hasUpper = false;
+  let hasDigit = false;
+
+  for (let char of str) {
+
+    if (char >= 'a' && char <= 'z') {
+      hasLower = true;
+    }
+
+    else if (char >= 'A' && char <= 'Z') {
+      hasUpper = true;
+    }
+
+    else if (char >= '0' && char <= '9') {
+      hasDigit = true;
+    }
+
+    else if (char === '_') {
+      continue;
+    }
+
+    else {
+      return false;
+    }
+  }
+
+  return hasLower && hasUpper && hasDigit;
+
+}
+
+
+function isValidUserPassword(str){
+    
+    if (str.length <= 7 || str.length >= 30) {
+    return false;
+    }
+
+    let hasLower = false;
+    let hasUpper = false;
+    let hasDigit = false;
+    let hasSpecial = false;
+    let hasSpace = false;
+
+    for (let char of str) {
+
+        if (char >= 'a' && char <= 'z') {
+        hasLower = true;
+        } 
+        else if (char >= 'A' && char <= 'Z') {
+        hasUpper = true;
+        } 
+        else if (char >= '0' && char <= '9') {
+        hasDigit = true;
+        } 
+        else if ("@&_$%?".includes(char)) {
+        hasSpecial = true;
+        } 
+        else if (char === ' ') {
+        hasSpace = true;
+        }
+    }
+
+    // Final check
+    return hasLower && hasUpper && hasDigit && hasSpecial && !hasSpace;
+}
+
 function register(event){
     event.preventDefault();
     let newUserName = document.getElementById("newUserName").value;
     let newUserPassword = document.getElementById("newPassword").value;
+    let validUserNam = isValidUserName(newUserName);
+    let validUserPassword = isValidUserPassword(newUserPassword)
+
+    if (!validUserPassword){
+        alert("Password must be 8–30 characters long and include at least one lowercase letter, one uppercase letter, one number, and one special character (@, &, _, $, %, or ?).");
+        return 
+    }
+
+    if(!validUserNam){
+        alert("Password must be 8–30 characters long and include at least one lowercase letter, one uppercase letter, one number, and one special character (_) special charector is optional.");
+        return 
+        
+    }
 
     let usersList = getUserFromLocalStorage();
 
